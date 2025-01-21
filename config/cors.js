b967400499b122af7e4fa2cf93c7aebe117bbbc1
DanielPazGithub <48155964+DanielPazGithub@users.noreply.github.com>
2025-01-20 22:43:33 -0300

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: 'https://spontaneous-otter-ac770c.netlify.app',
+  origin: '*',
 
   /*
   |--------------------------------------------------------------------------
